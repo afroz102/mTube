9826a059f4f9f37319d0442bdedf2486cf1f2a3c
md-afroz-vst-au4 <afrozjmi102@gmail.com>
2020-08-05 23:03:51 +0530

    second push

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const keys = {
+    API_KEY: "AIzaSyA9l824tY0ctTwcxH-rwt3BJtVPAeNPEnA",
+    BASE_URL: "https://www.googleapis.com/youtube/v3",
+
+    // not for use now 
+    CLIENT_ID: "116187113356-ev6gcutvosmbsiskactsv5vetsecampa.apps.googleusercontent.com",
+    CLIENT_SECRET: "OJqz7WhFmiMDh0Va9kpyUWcd"
+}
